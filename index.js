ed25b8cc9df31f2b29dec6334fb9b806116b05e5
Caitlin <cjcroteau@gmail.com>
2022-04-18 00:05:56 +0000

    add index.js. Need to fix validation

--- index.js
+++ index.js
@@ -1,29 +1,100 @@
-let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+
+  get balance() {
+    let balance = 0;
+    for (let t of this.transactions) {
+      balance += t.value;
+    }
+    return balance
+  }
+
+  addTransactions(transaction) {
+    this.transactions.push(transaction);
+  }
+
+}
+//need to fix validation - it's not working if you bump the balance up over 100
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+
+    //my attempt
+    // if (this.isAllowed()) {
+    //   this.time = new Date();
+    //   this.account.addTransactions(this);
+    // }
+    if (!this.isAllowed()) {
+      return false;
+    } else {
+      this.time = new Date();
+      this.account.addTransactions(this);
+      return true;
+    }
   }
 
 }
 
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+  //my attempt:
+  // isAllowed() {
+  //   if (this.account.balance + this.value <= 0) {
+  //     return false;
+  //   }
+  //   return true;
+  // }
+
+  isAllowed() {
+    return (this.account.blance - this.amount >= 0)
+
+  }
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+
+  isAllowed() {
+    return true;
+  }
+}
 
 
+
+const myAccount = new Account("snow-patrol");
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
+console.log('Account Balance: ', myAccount.balance)
 
-t2 = new Withdrawal(9.99);
+
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
+console.log('Account Balance:', myAccount.balance);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log("Transaction 3:", t3);
+console.log('Ending Balance: ', myAccount.balance);
